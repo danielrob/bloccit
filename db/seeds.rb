3aa0fa6bdad4a67819381206d2318864ce2324e5
Daniel Robinson <djr.self@gmail.com>
2015-04-08 12:48:08 -0400

    Seed a unique post and unique comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'faker'
- 
+
  # Create Posts
  50.times do
    Post.create!(
@@ -17,6 +17,23 @@ require 'faker'
    )
  end
  
+# Assignment
+if !Post.exists?(title: "unique post")
+  Post.create!(
+      title: "unique post",
+      body: "this data should be idempotent"
+    )
+end
+
+# Assignment
+if !Comment.exists?(body: "unique comment")
+  Comment.create!(
+      post: Post.find_by_title("unique post"),
+      body: "unique comment"
+    )
+end
+
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
