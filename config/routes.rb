b7260c60c4b42612fca76f43d91f130da9d4e8e4
Daniel Robinson <djr.self@gmail.com>
2015-04-08 13:11:24 -0400

    Add posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  
+  resources :posts
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root to: 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
