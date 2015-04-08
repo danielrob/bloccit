342b16e61c04bfa0ba6f5ebc21d60c5fb79e1759
Daniel Robinson <djr.self@gmail.com>
2015-04-08 19:15:41 -0400

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,15 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  default_scope { order('created_at DESC') }
+  # see http://stackoverflow.com/questions/1834159/overriding-a-rails-default-scope
+  #default_scope { order('created_at DESC') }
+
+
+  # Scope Syntax a)
+  def self.ordered_by_title
+    order(title: 'ASC')
+  end
+
+  # Scope syntax b)
+  scope :ordered_by_reverse_created_at, -> { order(created_at: :ASC)}
 end
