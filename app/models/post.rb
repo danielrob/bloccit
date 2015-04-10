d0fe0422f59593158c415acb829667f17c7f000c
Daniel Robinson <djr.self@gmail.com>
2015-04-10 14:22:40 -0400

    Complete uploading images assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
   default_scope { order('created_at DESC') }
+  mount_uploader :image, ImageUploader
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
